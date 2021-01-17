c50d94174d07ab4a01cf07f8cb90461c4bb446d3
Musawer Ali <saad.abid@ilsainteractive.com>
2021-01-17 12:34:23 +0500

    some improvements

--- quadricEquationSolver/main.py
+++ quadricEquationSolver/main.py
@@ -27,13 +27,7 @@ else:
         x2=(-b-sqrt(b**2-4*a*c))/(2*a)
         print("x1 = "+str(x1))
         print("x2 = "+str(x2))
-# x1 = (-b+sqrt(b**2-4*a*c))/(2*a)
-# x2 = (-b-sqrt(b**2-4*a*c))/(2*a)
 
-
-
-# print("x1 = "+str(x1))
-# print("x2 = "+str(x2))
 
 
 
